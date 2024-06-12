178ccedff61424f1ea23065e4b64f056d841f1ce
marisa <marisa.mayumi@aluno.ifsp.edu.br>
2024-06-12 15:16:50 -0300

    adicionado detalhes do ex2

--- 03-modulos-pacotes/exercicio/imc/calculo_imc.py
+++ 03-modulos-pacotes/exercicio/imc/calculo_imc.py
@@ -2,20 +2,20 @@ from imc.classificacao import classificacoes
 from math import pow
 
 def imc(peso, altura):
-    return round(peso / pow(altura, 2), 2)
+    return round(peso / pow(altura, 2), 1)
 
 def classificar(imc):
     for classe in classificacoes:
-        if imc > classificacoes[classe][0] and imc <= classificacoes[classe][1]:
+        if imc >= classificacoes[classe][0] and imc <= classificacoes[classe][1]:
             return classe
 
 def peso_a_ganhar_ou_perder(peso, altura):
     imc_normal = 24.9
     peso_normal = imc_normal * pow(altura, 2)
-    
+
     diferenca = peso - peso_normal
 
     if diferenca < 0:
-        return f'você precisa ganhar {abs(round(diferenca, 2))}'
+        return f'você precisa ganhar {abs(round(diferenca, 1))} kg'
     else:
-        return f'você precisa perder {round(diferenca, 2)}'
+        return f'você precisa perder {round(diferenca, 1)} kg'
