b105d03ca18fa0a052eb263cf7ce897cf7bdbbb5
maris <marisa.mayumi.mo@gmail.com>
2024-07-16 08:58:11 -0300

    adicionado 04-arquivos funções

--- 04-arquivos/arquivo.py
+++ 04-arquivos/arquivo.py
@@ -49,8 +49,8 @@ with open('04-arquivos/dados.txt', 'r') as arquivo:
 # w - write - substitui todo o conteudo
 # a - append - escreve ao final
 
-with open('04-arquivos/dados.txt', 'a') as arquivo:
-    arquivo.write('\njaca')
+# with open('04-arquivos/dados.txt', 'a') as arquivo:
+#     arquivo.write('\njaca')
     # quando tem pass, ele não fecha
 
     # em arquivo geralmente não apaga
@@ -64,10 +64,51 @@ with open('04-arquivos/dados.txt', 'r') as arquivo:
 
 # ----------------------------------------------------
 
-with open('04-arquivos/produtos.csv', 'r') as arquivo:
+# with open('04-arquivos/produtos.csv', 'r') as arquivo:
     # comma separated values
     # cada coluna representa um atributo do produto
     # cada linha é um elemento
 
     # um arquivo pra cada tabela
     
+    # é bom tratar o csv usando biblioteca 
+    # pra não fazer na mão
+    
+    # produtos = []
+    
+    # # 'in' utilizavel nos iteráveis, considera as linhas
+    # for produto in arquivo: 
+    #     produtos.append(produto.strip().split(sep=',')) # devolve dados com nome da lista
+
+    # print(produtos)
+def obter_produtos():
+    with open('04-arquivos/produtos.csv', 'r') as arquivo:
+        produtos = []
+        
+        # 'in' utilizavel nos iteráveis, considera as linhas
+        for produto in arquivo: 
+            # lista com todos os dados dessa linha
+            dados_produto = produto.strip().split(sep=',')
+
+            produto = {
+                'nome': dados_produto[0],
+                'preco': float(dados_produto[1]),
+                'marca': dados_produto[2]
+            }
+            produtos.append(produto)
+
+        return produtos
+
+def salvar_produto(nome, preco, marca):
+    with open('04-arquivos/produtos.csv', 'a') as arquivo:
+        linha = f'\n{nome},{preco},{marca}'
+        arquivo.write(linha)
+
+# criar abstração que faz algo
+# bom separar, pq melhora manutenção
+# reutilizavel
+
+
+salvar_produto('banana', 10, 'feira')
+print(obter_produtos())
+
